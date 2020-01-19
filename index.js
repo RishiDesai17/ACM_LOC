1180561458fe4434e555b48fb48ab9471688efcb
RishiDesai17 <rishindesai@gmail.com>
2020-01-19 18:47:51 +0530

    typing effect

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const blink = () => {
-    const x = document.getElementById("blink");
-    setInterval(()=>{
-        x.style.visibility = (x.style.visibility=="hidden"?"visible":"hidden")
-    },500);
+let i=0;
+const fn = () => {
+    const typing = () => {
+        const txt = "OF CODE ";
+        if (i < txt.length) {
+          document.getElementsByClassName("initial")[0].innerHTML += txt.charAt(i);
+          i++;
+          setTimeout(typing, 150);
+        }
+        else{
+            const x = document.getElementById("blink");
+            setInterval(()=>{
+                x.style.visibility = (x.style.visibility=="hidden"?"visible":"hidden")
+            },500);
+        }
+    }
+    typing();
+    
 }
+
